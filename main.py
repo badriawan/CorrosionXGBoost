b215de3fbae06ead84d182f8b8eea2c21a120d10
badriawan <yusufbadriawan@gmail.com>
2026-01-07 14:16:54 +0800

    corss_val

--- main.py
+++ main.py
@@ -119,10 +119,18 @@ print("Prediksi corrosion:", prediction)
 
 from sklearn.model_selection import cross_val_score
 
+# scores = cross_val_score(
+#     model, X, y,
+#     scoring='neg_root_mean_squared_error',
+#     cv=5
+# )
+
 scores = cross_val_score(
-    model, X, y,
-    scoring='neg_root_mean_squared_error',
-    cv=5
+    model,
+    X_train,
+    y_train,
+    scoring="neg_root_mean_squared_error",
+    cv=10
 )
 
 print("CV RMSE:", -scores.mean())
